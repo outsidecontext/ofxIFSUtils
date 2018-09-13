4d6be51186f335a28947116e4f86c95d2401be06
Chris Mullany <me@chrismullany.com>
2018-09-13 21:00:55 +0100

    Optional truetype font, added a coloured divider thingy

--- src/IFSUIUtils.cpp
+++ src/IFSUIUtils.cpp
@@ -2,13 +2,36 @@
 using namespace std;
 using namespace IFS;
 
+ofTrueTypeFont UiUtils::font;
+
+
+void UiUtils::DrawDivider(glm::vec2 & p, int width, int height, int paddingTop) {
+	p.y += paddingTop;
+	ofDrawRectangle(p, width, height);
+	p.y += height * 2;
+}
+
 void UiUtils::DrawLabel(const string & textString, glm::vec2 & p, int height) {
-	ofDrawBitmapString(textString, p);
+	if (font.isLoaded())
+	{
+		font.drawString(textString, p.x, p.y);
+	}
+	else
+	{
+		ofDrawBitmapString(textString, p);
+	}
 	p.y += height;
 }
 
 void UiUtils::DrawLabel(const string & textString, glm::vec2 & p, const ofColor & colour, const ofColor & bgColour, int height) {
-	ofDrawBitmapString(textString, p);
-	ofDrawBitmapStringHighlight(textString, p, bgColour, colour);
+	if (font.isLoaded())
+	{
+		font.drawString(textString, p.x, p.y);
+	}
+	else
+	{
+		ofDrawBitmapString(textString, p);
+		ofDrawBitmapStringHighlight(textString, p, bgColour, colour);
+	}
 	p.y += height;
 }
--- src/IFSUIUtils.h
+++ src/IFSUIUtils.h
@@ -6,6 +6,8 @@ namespace IFS
 {
 	class UiUtils {
 	public:
+		static ofTrueTypeFont font;
+		static void DrawDivider(glm::vec2 & p, int width = 100, int height = 20, int paddingTop = 10);
 		static void DrawLabel(const std::string & textString, glm::vec2 & p, int height = 20);
 		static void DrawLabel(const std::string & textString, glm::vec2 & p, const ofColor & colour, const ofColor & bgColour, int height = 20);
 	protected:
